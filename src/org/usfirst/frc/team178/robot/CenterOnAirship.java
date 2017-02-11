5a9f012eec679894458ee99bccf068d8b089c493
Hunter <huntercaouette@gmail.com>
2017-02-11 14:47:39 -0500

    rect r now writes to an array of two rectangles' worth of information.

--- src/org/usfirst/frc/team178/robot/CenterOnAirship.java
+++ src/org/usfirst/frc/team178/robot/CenterOnAirship.java
@@ -1,26 +1,64 @@
 package org.usfirst.frc.team178.robot;
 
+import org.usfirst.frc.team178.robot.Robot;
+import org.usfirst.frc.team178.robot.subsystems.DriveTrain;
 import org.usfirst.frc.team178.robot.VisionStreamer;
 
-import edu.wpi.first.wpilibj.*;
+import edu.wpi.first.wpilibj.RobotDrive;
 import edu.wpi.first.wpilibj.command.Command;
 
+
+/**
+ *
+ */
 public class CenterOnAirship extends Command {
-	
-	private RobotDrive drive;
-	private VisionStreamer frontCamera = new VisionStreamer("frontCamera", "10.1.78.109");
+
 
 	private final Object imgLock = new Object();
+	DriveTrain drivetrain;
+	VisionStreamer camera;
 	
-	
-	public void centerAndAdvance() {
-		double centerX;
+    public CenterOnAirship() {
+    	requires(Robot.drivetrain);
+    	drivetrain = Robot.drivetrain;
+    	requires(Robot.frontCamera);
+    	camera = Robot.frontCamera;
+        // Use requires() here to declare subsystem dependencies
+        // eg. requires(chassis);
+    }
+   
+    // Called just before this Command runs the first time
+    protected void initialize() {
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+    	double centerX;
 		synchronized (imgLock) {
-			centerX = frontCamera.getTapeCenterX();
+			centerX = camera.getBlendedCenterX();
 		}
-		double turn = centerX - (frontCamera.getIMG_WIDTH() / 2);
-		drive.arcadeDrive(-0.6, turn * 0.005);
-	}
-	
-	Command.isFinished();
+		double turn = centerX - (camera.getIMG_WIDTH() / 2);
+		drivetrain.drive(-0.6, turn * 0.005);
+		System.out.println("Turn: " + turn);
+		System.out.println("CenterX: " + centerX);
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+    	if (Math.abs(camera.getCenterXfromCameraCenterX()) < 10) {
+    		return true;
+    	}
+    	else {
+    		return false;
+    	}
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+    }
 }
--- src/org/usfirst/frc/team178/robot/VisionStreamer.java
+++ src/org/usfirst/frc/team178/robot/VisionStreamer.java
@@ -3,63 +3,87 @@ package org.usfirst.frc.team178.robot;
 import org.opencv.core.Mat;
 import org.opencv.core.Rect;
 import org.opencv.imgproc.Imgproc;
+import org.usfirst.frc.team178.robot.GripPipeline;
 import org.usfirst.frc.team178.robot.RobotMap;
+
 
 import edu.wpi.cscore.AxisCamera;
 import edu.wpi.first.wpilibj.CameraServer;
 import edu.wpi.first.wpilibj.RobotDrive;
 import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.vision.VisionPipeline;
 import edu.wpi.first.wpilibj.vision.VisionThread;
 
 public class VisionStreamer extends Subsystem {
 	
 	//actual values to be determined after final physical setup implemented
-	private static final int IMG_WIDTH = 320;
-	private static final int IMG_HEIGHT = 240;
+	private static final int IMG_WIDTH = 800;
+	private static final int IMG_HEIGHT = 600;
 	private VisionThread visionThread;
-	private double centerX = 0.0;
-	private double centerY = 0.0;
+	private double[] centerX = {0.0, 0.0};
+	private double[] centerY = {0.0, 0.0};
+	private double[] rectWidth = {0.0, 0.0};
+	private double[] rectHeight = {0.0, 0.0};
 	
 	private final Object imgLock = new Object();
 	
+	private AxisCamera camera;
+	
 	public VisionStreamer(String cameraName, String host) {
-	    AxisCamera camera = new AxisCamera(cameraName, host);
+	    camera = new AxisCamera(cameraName, host);
 	    camera.setResolution(IMG_WIDTH, IMG_HEIGHT);
 	    
 	    visionThread = new VisionThread(camera, new GripPipeline(), pipeline -> {
 	        if (!pipeline.filterContoursOutput().isEmpty()) {
-	            Rect r = Imgproc.boundingRect(pipeline.filterContoursOutput().get(0));
+	        	for (int i = 0; i < 2; i++) {
+	            Rect r = Imgproc.boundingRect(pipeline.filterContoursOutput().get(i));
 	            synchronized (imgLock) {
-	                centerX = r.x + (r.width / 2);
-	                centerY = r.y + (r.height / 2);
-	            }
+	                centerX[i] = r.x + (r.width / 2);
+	                centerY[i] = r.y + (r.height / 2);
+	                rectWidth[i] = r.width;
+	                rectHeight[i] = r.height;
+	            	}
+	        	}
 	        }
 	    });
 	    visionThread.start();
 	}
+	
 	
 	/*alternative to implementing a thread 
 	 * public void processCurrentImage() {
 		GripPipeline.process();
 	}*/
 	
-	public double getTapeCenterX(){
-		return centerX;
+	public double getBlendedCenterX(){
+		return ((centerX[0]+centerX[1])/2);
 	}
 	public double getCenterXfromCameraCenterX(){
-		return ((IMG_WIDTH/2)-centerX);
+		return ((IMG_WIDTH/2) - getBlendedCenterX());
 	}
-	public double getTapeCenterY(){
-		return centerY;
+	public double getBlendedCenterY(){
+		return ((centerY[0]+centerY[1])/2);
 	}
 	public double getCenterYfromCameraCenterY(){
-		return ((IMG_HEIGHT/2)-centerY);
+		return ((IMG_HEIGHT/2) - getBlendedCenterY());
 	}
 	public double getIMG_WIDTH(){
 		return IMG_WIDTH;
 	}
 	public double getIMG_HEIGHT(){
 		return IMG_HEIGHT;
+	}
+	public double getAirshipWidth(){
+		return ((Math.abs(centerX[1]-centerX[0]))+(rectWidth[1]/2)+(rectWidth[0]/2));
+	}
+	public double getAirshipHeight(){
+		return ((rectHeight[0]+rectHeight[1])/2);
+	}
+	public double getBoilerWidth(){
+		return ((rectWidth[0]+rectWidth[1])/2);
+	}
+	public double getBoilerHeight(){
+		return ((Math.abs(centerY[1]-centerY[0]))+(rectHeight[1]/2)+(rectHeight[0]/2));
 	}
 	
 	
